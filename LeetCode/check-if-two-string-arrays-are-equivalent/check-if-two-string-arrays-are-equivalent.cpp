bfe2a2dee876bb0190333f347af21fed8790eab5
devloper-j <phoolchandj35@gmail.com>
2026-09-06 11:51:21 +0530

    feat(leetcode): add check-if-two-string-arrays-are-equivalent solution

---
+++ LeetCode/check-if-two-string-arrays-are-equivalent/check-if-two-string-arrays-are-equivalent.cpp
@@ -0,0 +1,9 @@
+        }
+        for (int i=0;i<word2.size();i++){
+            y+=word2[i];
+        }
+        return x==y;
+            x+=word1[i];
+        for (int i=0;i<word1.size();i++){
+        string y ="";
+        string x ="";
